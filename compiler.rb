b0ff0f45c33f9e468407453e060b206fad897ed0
Andrew Couch <andy@couchand.com>
2012-12-22 00:48:14 -0500

    compiler: acceptance test - fails

---
+++ compiler.rb
@@ -0,0 +1,12 @@
+module Gt2
+  module Compiler
+    class Treetop
+      def self.compiler
+        Treetop.new
+      end
+
+      def compile(input)
+      end
+    end
+  end
+end
---
+++ test/compiler_treetop.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require_relative '../compiler.rb'
+
+module Gt2
+  module Compiler
+    class TreetopTest < Test::Unit::TestCase
+      def test_compile_gt2
+        input = File.open('bnf.bnf').read
+        expected = File.open('bnf.treetop').read
+        compiler = Treetop.compiler
+
+        actual = compiler.compile input
+
+        assert_equal expected, actual, 'We should eat our own dog food'
+      end
+    end
+  end
+end
